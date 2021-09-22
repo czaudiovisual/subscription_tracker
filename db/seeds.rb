9e8dafc84c3de236eeff1778e7fba6059de86d4c
Christian Bermeo <czaudiovisual@hotmail.com>
2021-09-22 19:37:12 -0400

    generate user resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Christian')
+User.create(username: 'Hunter')
+User.create(username: 'Caroline')
+User.create(username: 'Michael')
